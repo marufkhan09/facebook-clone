8c5a0d85918498c80bafe1e7bbc2972396e90909
marufkhan09 <maruf1609@gmail.com>
2024-07-29 16:36:53 +0600

    common tabbar added

---
+++ facebook/Core/TabBar/MainTabView.swift
@@ -0,0 +1,24 @@
+//
+//  MainTabView.swift
+//  facebook
+//
+//  Created by Maruf Khan on 29/7/24.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            Text("Home").tabItem { Image(systemName: "house") }
+            Text("Video").tabItem { Image(systemName: "play.tv") }
+            Text("Friends").tabItem { Image(systemName: "person.2") }
+            Text("Marketplace").tabItem { Image(systemName:"scribble") }
+            Text("Menu").tabItem { Image(systemName: "line.3.horizontal") }
+        }
+    }
+}
+
+#Preview {
+    MainTabView()
+}
