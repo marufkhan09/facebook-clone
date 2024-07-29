bea9e1e88aa65628d81cbdd615b936ca9c4bd80b
marufkhan09 <maruf1609@gmail.com>
2024-07-29 15:58:11 +0600

    Initial Commit

---
+++ facebook/facebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  facebookApp.swift
+//  facebook
+//
+//  Created by Maruf Khan on 29/7/24.
+//
+
+import SwiftUI
+
+@main
+struct facebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
